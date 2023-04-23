a6fb73decabd9172f854285b852a4609c3897add
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-23 19:02:48 +0200

    refactor(3.4. Tag selectors): delete tag selectors

--- app.js
+++ app.js
@@ -15,32 +15,38 @@ var completedTasksHolder = document.getElementById("completed-tasks"); //complet
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement("li");
+  listItem.classList.add("block-task");
 
   //input (checkbox)
-  var checkBox = document.createElement("input"); //checkbx
+  var checkBox = document.createElement("input"); //checkbox
+  checkBox.classList.add("input-task");
   //label
   var label = document.createElement("label"); //label
+  label.classList.add("label-task");
   //input (text)
   var editInput = document.createElement("input"); //text
+  editInput.classList.add("input-task");
   //button.edit
   var editButton = document.createElement("button"); //edit button
-
+  editButton.classList.add("button-task");
   //button.delete
   var deleteButton = document.createElement("button"); //delete button
+  deleteButton.classList.add("button-task");
   var deleteButtonImg = document.createElement("img"); //delete button image
+  deleteButtonImg.classList.add("img-remove");
 
   label.innerText = taskString;
-  label.className = "task";
+  label.classList.add("task");
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.classList.add("task");
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.classList.add("edit");
 
-  deleteButton.className = "delete";
+  deleteButton.classList.add("delete");
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
